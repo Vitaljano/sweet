fae6bc75a71b2ff164a27008e4c9a258d791959d
Vitaljano <lazarev88@gmail.com>
2018-12-31 10:57:27 +0200

    add search function
    basic web page

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@ let cart = document.querySelector(".cart");
 cartBtn.addEventListener("click", function(){
     cart.classList.toggle("show-cart");
 });
+
+// search box 
+
+const searchBar = document.querySelector("#search-box");
+
+
+searchBar.addEventListener("keyup", function(e){
+    const term = e.target.value.toLowerCase();
+    const items = document.querySelectorAll(".sweets");
+
+    Array.from(items).forEach(function(item){
+        const title = item.querySelector(".cart-title").textContent.toLowerCase();
+        if(title.indexOf(term) != -1 ){
+            item.style.display = "block";
+            console.log(item);
+        } else {
+            item.style.display = "none";
+        }
+
+    });
+
+
+});
